24a742f94b16a157697ab3855fd0dcce3c270467
river-flows-in-ryu <rgr65066384@gmail.com>
2024-05-12 20:54:56 +0900

    장바구니 체크박스 기능 추가

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,17 +1,15 @@
 "use client";
 import React, { useEffect, useState } from "react";
 
-import Image from "next/image";
-
 import Container from "@/components/container";
 
 import { useUserIdStore } from "@/app/store";
 import { commonFetch } from "@/utils/commonFetch";
-import Link from "next/link";
 
-import close from "../../public/close.png";
 import Modal from "@/components/modal";
 import CartDeleteModal from "@/components/cartDeleteModal";
+
+import CartProductItem from "@/components/cartProductItem";
 
 export default function Page() {
   const { userId } = useUserIdStore();
@@ -22,18 +20,29 @@ export default function Page() {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedProductId, setSelectedProductId] = useState(0);
 
-  console.log(cartItems);
+  const [checkedItems, setCheckedItems] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       if (userId) {
-        const res = await commonFetch(
-          `${process.env.NEXT_PUBLIC_API_URL}/cart/${userId}`,
-          "get"
-        );
-        if (res.Message === "SUCCESS") {
-          setTotalCount(res.totalCount);
-          setCartItems(res.data);
+        try {
+          const res = await commonFetch(
+            `${process.env.NEXT_PUBLIC_API_URL}/cart/${userId}`,
+            "get"
+          );
+          if (res.Message === "SUCCESS") {
+            setTotalCount(res.totalCount);
+            const copyArray = structuredClone(res?.data);
+            const addCheckedArray = copyArray?.map((item) => ({
+              ...item,
+              index: `${item?.product?.id}` + `${item?.size_attribute?.id}`,
+            }));
+            console.log(addCheckedArray);
+            setCheckedItems(addCheckedArray?.map((item) => item?.index));
+            setCartItems(addCheckedArray);
+          }
+        } catch (e) {
+          console.log(e);
         }
       }
     };
@@ -45,6 +54,28 @@ export default function Page() {
     setSelectedProductId(productId);
   };
 
+  const handleCheckboxChange = (index: string) => {
+    setCheckedItems((checkedItems) => {
+      if (checkedItems.includes(index)) {
+        return checkedItems.filter((item) => item !== index);
+      } else {
+        return [...checkedItems, index];
+      }
+    });
+  };
+
+  const handleClickAllCheck = () => {
+    if (checkedItems.length === totalCount) {
+      setCheckedItems([]);
+    } else {
+      setCheckedItems(
+        cartItems?.map(
+          (item) => `${item?.product?.id}` + `${item?.size_attribute?.id}`
+        )
+      );
+    }
+  };
+
   return (
     <Container>
       <Modal
@@ -54,63 +85,31 @@ export default function Page() {
       />
       <div className="w-full h-2.5 bg-[#f2f2f2] " />
       <div className=" w-full h-10 bg-white text-center leading-10">
+        <input
+          type="checkbox"
+          className={`w-5 h-5 checked:bg-primary border border-[#ccc] checked:border-primary bg-[#fff] rounded-full mr-2.5 appearance-none checked:bg-checkedWhite 	`}
+          onChange={handleClickAllCheck}
+          checked={checkedItems.length === totalCount}
+          style={{
+            backgroundImage: "url('/check_white.png')",
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "center",
+            backgroundSize: "contain",
+          }}
+        />
         총 장바구니 갯수 <span className="text-primary">{totalCount}</span> 개
       </div>
       <div className="w-full h-2.5 bg-[#f2f2f2] " />
       <div className="w-full">
         <div className="w-full h-full px-[15px] pt-6 pb-5">
-          {cartItems.map((item, index) => (
-            <div key={index} className="flex mb-5">
-              <button className="w-5 h-5 mr-2.5">v</button>
-              <div className="w-[70px] ">
-                <Link href={`/goods/${item?.product?.id}`}>
-                  <Image
-                    src={item?.product?.image_url}
-                    alt=""
-                    width={100}
-                    height={100}
-                  />
-                </Link>
-              </div>
-              <div className="ml-2.5 w-[calc(100%-110px)]">
-                <div className="flex justify-between">
-                  <div className="flex flex-col leading-[18px]  w-[calc(100%-20px)]">
-                    {/* todo */}
-                    <Link href={`/goods/${item?.product?.id}`}>
-                      <span className="text-xs">
-                        {item?.product?.user?.username}
-                      </span>
-                    </Link>
-                    <Link href={`/goods/${item?.product?.id}`}>
-                      <span className=" font-bold text-xs line-clamp-1	">
-                        {item?.product?.productName}
-                      </span>
-                    </Link>
-                  </div>
-                  <button
-                    className="w-5 h-5"
-                    onClick={() => handleClickDelete(item?.product?.id)}
-                  >
-                    <Image src={close} alt="close" width={20} height={20} />
-                  </button>
-                </div>
-                <div className="flex justify-between text-xs text-[#6e6e6e]">
-                  <span>
-                    수량 {item?.quantity} | {item?.size_attribute?.size?.name}
-                  </span>
-                  <span className="line-through">
-                    {item?.product?.OriginPrice?.toLocaleString()}원
-                  </span>
-                </div>
-                <div className="flex justify-between mt-1 ">
-                  <button className="border border-secondary text-sm px-2.5">
-                    옵션/수량
-                  </button>
-                  <span className="text-bold text-black">
-                    {item?.product?.discountPrice?.toLocaleString()}원
-                  </span>
-                </div>
-              </div>
+          {cartItems.map((item) => (
+            <div key={`${item?.product?.id + item?.size_attribute?.id}`}>
+              <CartProductItem
+                item={item}
+                handleClickDelete={handleClickDelete}
+                checkedItems={checkedItems}
+                handleCheckboxChange={handleCheckboxChange}
+              />
             </div>
           ))}
         </div>
---
+++ components/cartProductItem.tsx
@@ -0,0 +1,106 @@
+import React from "react";
+
+import Image from "next/image";
+import Link from "next/link";
+
+import close from "../public/close.png";
+
+interface Props {
+  item: PropsItems;
+  handleClickDelete: (productId: number) => void;
+  checkedItems: string[];
+  handleCheckboxChange: (index: string) => void;
+}
+
+interface PropsItems {
+  product: {
+    id: number;
+    image_url: string;
+    productName: string;
+    OriginPrice: number;
+    discountPrice: number;
+    user: {
+      username: string;
+    };
+  };
+  size_attribute: {
+    id: number;
+    name: string;
+    size: {
+      name: string;
+    };
+  };
+  quantity: number;
+  checked: boolean;
+}
+
+export default function CartProductItem({
+  item,
+  handleClickDelete,
+  checkedItems,
+  handleCheckboxChange,
+}: Props) {
+  return (
+    <div className="flex mb-5 ">
+      <input
+        type="checkbox"
+        className={`w-5 h-5   checked:bg-primary border border-[#ccc] checked:border-primary bg-[#fff] rounded-full mr-2.5 appearance-none checked:bg-checkedWhite 	`}
+        checked={checkedItems.includes(item?.index)}
+        onChange={() => handleCheckboxChange(item?.index)}
+        style={{
+          backgroundImage: "url('/check_white.png')",
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+          backgroundSize: "contain",
+        }}
+      />
+      <div className="w-[70px] ">
+        <Link href={`/goods/${item?.product?.id}`}>
+          <Image
+            src={item?.product?.image_url}
+            alt={item?.product?.productName}
+            width={70}
+            height={70}
+          />
+        </Link>
+      </div>
+      <div className="ml-2.5 w-[calc(100%-110px)]">
+        <div className="flex justify-between">
+          <div className="flex flex-col leading-[18px]  w-[calc(100%-20px)]">
+            {/* todo */}
+            <Link href={`/goods/${item?.product?.id}`}>
+              <span className="text-xs">{item?.product?.user?.username}</span>
+            </Link>
+            <Link href={`/goods/${item?.product?.id}`}>
+              <span className=" font-bold text-xs line-clamp-1	">
+                {item?.product?.productName}
+              </span>
+            </Link>
+          </div>
+          <button
+            className="w-5 h-5"
+            onClick={() => handleClickDelete(item?.product?.id)}
+          >
+            <Image src={close} alt="close" width={20} height={20} />
+          </button>
+        </div>
+        <div className="flex justify-between text-xs text-[#6e6e6e]">
+          <span>
+            수량 {item?.quantity} | {item?.size_attribute?.size?.name}
+          </span>
+          <span className="line-through">
+            {item?.product?.OriginPrice?.toLocaleString()}원
+          </span>
+        </div>
+        <div className="flex justify-between mt-1 ">
+          <button className="border border-secondary text-sm px-2.5">
+            옵션/수량
+          </button>
+          <span className="text-bold text-black">
+            {item?.product?.discountPrice?.toLocaleString()}원
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+}
