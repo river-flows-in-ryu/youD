9320714a136fa929a11abb80912e82a467e492d8
river-flows-in-ryu <rgr65066384@gmail.com>
2024-09-12 16:25:34 +0900

    비로그인 유저도 쿠폰 확인 가능

--- app/goods/[slug]/page.tsx
+++ app/goods/[slug]/page.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 
+import { cookies } from "next/headers";
+
 import { commonFetch } from "@/utils/commonFetch";
+import decodingJwttsx from "@/utils/decodingJwt";
 
 import Client from "./client";
 
 export default async function Page({ params }: { params: { slug: string } }) {
+  const cookieStore = cookies();
+  const access_token = cookieStore.get("access_token");
+  const decodingJwt = decodingJwttsx(access_token?.value);
+  const userId = decodingJwt?.user_id;
+
+  const { slug } = params;
+
   async function getProductData(slug: string) {
     try {
       const res = commonFetch(
@@ -32,13 +42,31 @@ export default async function Page({ params }: { params: { slug: string } }) {
     }
   }
 
-  const productData = await getProductData(params?.slug);
-  const reviewData = await getReviewData(params?.slug);
+  async function getProductCoupons(slug: string) {
+    try {
+      const userIdValue = userId ?? 0;
+      const res = commonFetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/coupons/products/${slug}/${userIdValue}`,
+        "get"
+      );
+      return res;
+    } catch (error) {
+      if (error instanceof Error) {
+        alert(error?.message);
+      }
+    }
+  }
+
+  const productData = await getProductData(slug);
+  const reviewData = await getReviewData(slug);
+  const productCouponsdata = await getProductCoupons(slug);
+  console.log(userId);
   return (
     <Client
       productData={productData}
       slug={params?.slug}
       reviewData={reviewData}
+      productCouponsdata={productCouponsdata?.results}
     />
   );
 }
